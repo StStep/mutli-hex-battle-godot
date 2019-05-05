29b397bc223510a7a2016cf112b07eaac28eddd8
R. Stephens Summerlin <st.summerlin@gmail.com>
2019-05-05 17:05:37 -0400

    Added limit to number of units that can be made

--- Deploy.gd
+++ Deploy.gd
@@ -1,18 +1,29 @@
 extends Node
 
 onready var nd_unit = preload("res://units//Unit.tscn")
+onready var nd_linecount  = get_node("CanvasLayer/UnitPalette/LineCount")
+onready var nd_troopcount  = get_node("CanvasLayer/UnitPalette/TroopCount")
+onready var nd_regcount  = get_node("CanvasLayer/UnitPalette/RegimentCount")
+
 var hexGrid = preload("res://addons/romlok.GDHexGrid/HexGrid.gd").new()
 
 var busy = false
+var linecnt = 6
+var troopcnt = 4
+var regcnt = 2
 
 func _ready():
 	hexGrid.hex_scale = Vector2(108.55, 108.55)
+	nd_linecount.text = str(linecnt)
+	nd_troopcount.text = str(troopcnt)
+	nd_regcount.text = str(regcnt)
 
 func _on_Finish_Deployment_pressed():
 	get_tree().change_scene("res://TurnX.tscn")
 
 func _on_BtMakeLine_pressed():
 	if busy: return
+	if linecnt <= 0: return
 	var line = nd_unit.instance()
 	line.set_as_line(hexGrid)
 	line.dragging = true
@@ -20,9 +31,12 @@ func _on_BtMakeLine_pressed():
 	line.connect("drag_started", self, "_start_dragging")
 	line.connect("drag_ended", self, "_stop_dragging")
 	_start_dragging(line)
+	linecnt -= 1
+	nd_linecount.text = str(linecnt)
 
 func _on_BtMakeTroop_pressed():
 	if busy: return
+	if troopcnt <= 0: return
 	var troop = nd_unit.instance()
 	troop.set_as_troop(hexGrid)
 	troop.dragging = true
@@ -30,9 +44,12 @@ func _on_BtMakeTroop_pressed():
 	troop.connect("drag_started", self, "_start_dragging")
 	troop.connect("drag_ended", self, "_stop_dragging")
 	_start_dragging(troop)
+	troopcnt -= 1
+	nd_troopcount.text = str(troopcnt)
 
 func _on_BtMakeRegiment_pressed():
 	if busy: return
+	if regcnt <= 0: return
 	var regiment = nd_unit.instance()
 	regiment.set_as_regiment(hexGrid)
 	regiment.dragging = true
@@ -40,6 +57,8 @@ func _on_BtMakeRegiment_pressed():
 	regiment.connect("drag_started", self, "_start_dragging")
 	regiment.connect("drag_ended", self, "_stop_dragging")
 	_start_dragging(regiment)
+	regcnt -= 1
+	nd_regcount.text = str(regcnt)
 
 func _start_dragging(unit):
 	busy = true
